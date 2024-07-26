03dd3c5983aa85e7abf82b419e7276b999cf8048
Diego Santisteban <kmillionary@gmail.com>
2024-07-26 11:24:25 -0600

    chore: Update proxy target URL in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://zerobolas.com",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
